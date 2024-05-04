da03b8dc519e513b210c4628f8ab65f10de254ad
Shaq-Mg <shaqmcg0@gmail.com>
2024-05-04 14:51:54 +0100

    Card Model created

---
+++ Model/Card.swift
@@ -0,0 +1,40 @@
+//
+//  Card.swift
+//  Meworry
+//
+//  Created by Shaquille McGregor on 04/05/2024.
+//
+
+import Foundation
+import SwiftUI
+
+class Card: Identifiable, ObservableObject {
+    var id = UUID()
+    var text: String
+    @Published var isFaceUp = false
+    @Published var isMatched = false
+    
+    init(text: String) {
+        self.text = text
+    }
+    func turnOver() {
+        self.isFaceUp.toggle()
+    }
+}
+
+let cardValues = [
+"🍉", "🥭", "🍏", "🍎", "🍇", "🥝", "🍋", "🍒", "🍍", "🍓", "🍊", "🥥",
+]
+
+func createCardList() -> [Card] {
+    //create a blank list
+    var cardList = [Card]()
+    
+    for cardValue in cardValues {
+        cardList.append(Card(text: cardValue))
+        cardList.append(Card(text: cardValue))
+    }
+    return cardList
+}
+
+let faceDown = Card(text: "?")
