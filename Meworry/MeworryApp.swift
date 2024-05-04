63b3785d0c9b0392fd99f81b30b71e382d1de39b
Shaq-Mg <shaqmcg0@gmail.com>
2024-05-04 14:38:52 +0100

    Initial Commit

---
+++ Meworry/MeworryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeworryApp.swift
+//  Meworry
+//
+//  Created by Shaquille McGregor on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MeworryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
